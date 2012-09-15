fabeb3265b8ab28b737dc77cce954aa53e3d8291
Jan <krimdomu@googlemail.com>
2012-09-15 23:50:58 +0200

    initial commit

---
+++ lib/DBIx/ORMapper/Migration.pm
@@ -0,0 +1,118 @@
+#
+# (c) Jan Gehring <jan.gehring@gmail.com>
+# 
+# vim: set ts=3 sw=3 tw=0:
+# vim: set expandtab:
+   
+package DBIx::ORMapper::Migration;
+
+our $VERSION = "0.1";
+   
+use strict;
+use warnings;
+use Data::Dumper;
+
+use DBIx::ORMapper::Migration::Table;
+
+require Exporter;
+
+use base qw(Exporter);
+
+use vars qw(@EXPORT);
+@EXPORT = qw(create_table drop_table add_column drop_column add_index drop_index sql);
+
+sub new {
+   my $that = shift;
+   my $proto = ref($that) || $that;
+   my $self = { @_ };
+
+   bless($self, $proto);
+
+   return $self;
+}
+
+#
+# EXPORTED FUNCTIONS
+#
+
+sub create_table(&) {
+   my ($code) = @_;
+   my $t = DBIx::ORMapper::Migration::Table->new;
+
+   &$code($t);
+
+   my $stmt = $t->get_statement();
+   $stmt->execute;
+}
+
+sub drop_table {
+   my ($table) = @_;
+   my $t = DBIx::ORMapper::Migration::Table->new;
+
+   my $code = sub {
+      my $t = shift;
+      $t->name($table);
+      $t->drop;
+   };
+
+   &$code($t);
+
+   my $stmt = $t->get_statement();
+   $stmt->execute;
+}
+
+sub add_column {
+   my ($table, $col_name, $col_type, $col_opts) = @_;
+   my $t = DBIx::ORMapper::Migration::Table->new;
+
+   my $code = sub {
+      my $t = shift;
+      $t->name($table);
+      $t->add_column($col_name, $col_type, $col_opts);
+   };
+
+   &$code($t);
+
+   my $stmt = $t->get_statement();
+   $stmt->execute;
+}
+
+sub drop_column {
+   my ($table, $col_name) = @_;
+   my $t = DBIx::ORMapper::Migration::Table->new;
+
+   my $code = sub {
+      my $t = shift;
+      $t->name($table);
+      $t->drop_column($col_name);
+   };
+
+   &$code($t);
+
+   my $stmt = $t->get_statement();
+   $stmt->execute;
+}
+
+sub add_index {
+   my ($table, $col_name) = @_;
+   my $t = DBIx::ORMapper::Migration::Table->new;
+
+   my $code = sub {
+      my $t = shift;
+      $t->name($table);
+      $t->add_index($col_name);
+   };
+
+   &$code($t);
+
+   my $stmt = $t->get_statement();
+   $stmt->execute;
+}
+
+sub drop_index {
+}
+
+sub sql {
+}
+
+1;
---
+++ lib/DBIx/ORMapper/Migration/Table.pm
@@ -0,0 +1,133 @@
+#
+# (c) Jan Gehring <jan.gehring@gmail.com>
+# 
+# vim: set ts=3 sw=3 tw=0:
+# vim: set expandtab:
+   
+package DBIx::ORMapper::Migration::Table;
+   
+use strict;
+use warnings;
+
+use DBIx::ORMapper::Helper;
+
+sub new {
+   my $that = shift;
+   my $proto = ref($that) || $that;
+   my $self = { @_ };
+
+   bless($self, $proto);
+
+   # default: create
+   $self->{'__get_statement__'} = sub { 
+      my $self = shift;
+      my $stmt = CREATE()
+                     ->table($self->{name})
+                        ->fields();
+
+      for my $field (@{$self->{'__fields'}}) {
+         my $name = $field->{name};
+         $stmt->$name($field->{type}, %{ $field->{args} });
+      }
+
+      $stmt = $stmt->end();
+
+      return $::db->get_statement($stmt);
+   };
+
+   return $self;
+}
+
+sub name {
+   my ($self, $name) = @_;
+   $self->{name} = $name;
+
+   return $self;
+}
+
+sub drop {
+   my ($self) = @_;
+
+   $self->{'__get_statement__'} = sub {
+      my $self = shift;
+      my $stmt = DROP()->table($self->{name});
+      return $::db->get_statement($stmt);
+   };
+}
+
+sub add_column {
+   my ($self, $col_name, $col_type, $col_opts) = @_;
+
+   $col_type = "\u$col_type";
+   $col_opts ||= {};
+   
+   $self->{'__get_statement__'} = sub {
+      my $self = shift;
+      my $stmt = ALTER()
+                  ->table($self->{name})
+                     ->add()
+                        ->column()
+                           ->$col_name($col_type, %{ $col_opts })
+                        ->end();
+
+      return $::db->get_statement($stmt);
+   };
+}
+
+sub drop_column {
+   my ($self, $col_name) = @_;
+
+   $self->{'__get_statement__'} = sub {
+      my $self = shift;
+      my $stmt = ALTER()
+                  ->table($self->{name})
+                     ->drop()
+                        ->column()
+                           ->$col_name()
+                        ->end();
+
+      return $::db->get_statement($stmt);
+   };
+}
+
+sub add_index {
+   my ($self, $col_name) = @_;
+
+   $self->{'__get_statement__'} = sub {
+      my $self = shift;
+      my $stmt = CREATE()->index("idx_$col_name")
+                              ->on($self->{name} => [$col_name]);
+
+      return $::db->get_statement($stmt);
+   };
+}
+
+sub get_statement {
+   my ($self) = @_;
+
+   my $code = $self->{'__get_statement__'};
+   return &$code($self);
+}
+
+sub AUTOLOAD {
+   use vars qw($AUTOLOAD);
+   my $self = shift;
+   
+   return $self if( $AUTOLOAD =~ m/DESTROY/ );
+   
+   $AUTOLOAD =~ m/^DBIx::ORMapper::Migration::Table::(.*?)$/;
+   my $col_name = shift;
+   my $col_type = "\u$1";
+   my $col_args = { @_ };
+   
+   push(@{$self->{'__fields'}}, {
+      name => $col_name,
+      type => $col_type,
+      args => $col_args
+   });
+
+   return $self;
+}
+
+
+1;
